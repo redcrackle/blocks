496265adde236eb9b218c5740110945639e4403c
Taher Jodhpurwala <taher.j@redcrackle.com>
2015-10-14 07:07:44 +0530

    Minor JQuery fixes and removed unwanted files

--- sites/all/modules/custom/stripe_payment/js/plan.js
+++ sites/all/modules/custom/stripe_payment/js/plan.js
@@ -2,7 +2,7 @@ jQuery(function($) {
     $( "#popup-element-0-active" ).click(function() {
         var input = $( "#stripe-payment-form,#pay_plan" );
         var value = input.val();
-        if(value === ''){
+        if( value === '' || value === 'professional' || value ==='vip' ){
             input.val( value + "standard" );
         }
     });
@@ -10,7 +10,7 @@ jQuery(function($) {
     $( "#popup-element-1-active" ).click(function() {
         var input = $( "#stripe-payment-form--2,#pay_plan" );
         var value = input.val();
-        if(value === ''){
+        if( value === '' || value === 'standard' || value ==='vip' ){
             input.val( "professional" );
         }
     });
@@ -18,7 +18,7 @@ jQuery(function($) {
     $( "#popup-element-2-active" ).click(function() {
         var input = $( "#stripe-payment-form--3,#pay_plan" );
         var value = input.val();
-        if(value === ''){
+        if( value === '' || value === 'standard' || value ==='professional' ){
             input.val( "vip" );
         }
     });
